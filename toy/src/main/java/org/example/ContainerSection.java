2399c0455745b213f2f7397c33ef6eaec56e7ca5
delalieuxmatt <matt.delalieux@icloud.com>
2025-10-08 11:41:24 +0200

    Carrier, container and storage section

---
+++ toy/src/main/java/org/example/ContainerSection.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ContainerSection {
+    public int id, storageID, width, height;
+    public ContainerSection(int id, int storageID){
+        this.id = id;
+        this.storageID = storageID;
+        width = 2;
+        height = 4;
+    }
+
+}
+
--- toy/src/main/java/org/example/StorageSection.java
+++ toy/src/main/java/org/example/StorageSection.java
@@ -2,35 +2,21 @@
 package org.example;
 
 public class StorageSection {
-    private int id, x, y;
-    private static final int WIDTH = 2;
-    private static final int HEIGHT = 4;
+    public int id, x, y;
+    public int width;
+    public int height;
 
     public StorageSection(int id, int x, int y) { // Constructor takes a Point
         this.id = id;
         this.x = x;
         this.y = y;
+        width = 2;
+        height = 4;
+
     }
 
     // Getters
-    public int getId() {
-        return id;
-    }
 
-    public int getX() { // Returns a Point object
-        return x;
-    }
-    public int getY() { // Returns a Point object
-        return y;
-    }
-
-    public int getWidth() {
-        return WIDTH;
-    }
-
-    public int getHeight() {
-        return HEIGHT;
-    }
 
     // Optional: toString for easy printing
     @Override
@@ -38,7 +24,7 @@ public class StorageSection {
         return "Storage{" +
                 "id=" + id +
                 ", bottomLeft=" + x + ',' + y  +
-                ", dimensions=" + WIDTH + "x" + HEIGHT +
+                ", dimensions=" + width + "x" + height +
                 '}';
     }
 }
