49bf3d865d4bbf2956a7e58cf770163e88b140d8
thibaut-beck <thibaut.beckske@gmail.com>
2025-10-08 10:49:35 +0200

    inlezen van alles en terug uitprinten

--- toy/src/main/java/org/example/Main.java
+++ toy/src/main/java/org/example/Main.java
@@ -1,17 +1,22 @@
 package org.example;
+import java.nio.charset.StandardCharsets;
+import java.io.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) throws IOException {
+        try (InputStream is = Main.class.getResourceAsStream("/toy.txt");
+             BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+
+            String content = sb.toString();
+            System.out.println(content);
         }
     }
 }
