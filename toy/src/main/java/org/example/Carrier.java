9180840c89a9ffc03c55d8a2c84db1b5bfa2da88
thibaut-beck <thibaut.beckske@gmail.com>
2025-10-18 22:08:52 +0200

    roteren update

--- toy/src/main/java/org/example/Carrier.java
+++ toy/src/main/java/org/example/Carrier.java
@@ -1,21 +1,52 @@
 package org.example;
 import java.util.ArrayList;
+
+/***
+ * Direction
+ * direction up = 1, down = 3       -> oneven : omhoog/omlaag
+ * direction Right = 2 , Left = 4   -> even   : rechts/links
+ * beginnen met direction 1 -> Omhoog
+ *
+ *
+ * wanneer mogen roteren -> roteren mag pas vanaf we uit de rijen rijden
+ *
+ */
 public class Carrier {
-    public int id, craneID, x, y, width, height, orientation;
+    public int id, craneID, x, y, width, height , direction;
+
+
     public ArrayList<Log> logs = new ArrayList<>();
     public Carrier(int id, int craneID, int x, int y){
         this.id = id;
         this.craneID = craneID;
         this.x = x;
         this.y = y;
-        width = 4;
-        height = 8;
-        orientation = 0;
+        this.width = 4;
+        this.height = 8;
+        this.direction = 1;
     }
-    public void rotate(){
-        width = 8;
-        height = 4;
-        orientation = 1;
+    public void rotate(int dir , int time) {
+        if (this.direction == dir) System.out.println(" je kan geen nul graden roteren");
+        else if (this.direction %2 == dir%2) System.out.println("Dit kan niet , 180 graden omdraaien carrier kan niet");
+        else if (this.direction %2 == 0) { // rechts, links -> boven/beneden
+            width = 4;
+            height = 8;
+            this.direction = dir;
+            // doordat we verdraaien veranderd ons onderste punt ook
+            this.x += 2;
+            this.y -= 2;
+            logs.add(new RotateLog(time , dir));
+
+        } else { // boven, beneden -> rechts links
+            width = 8;
+            height = 4;
+            this.direction = dir;
+            // doordat we verdraaien veranderd ons onderste punt ook
+            this.x -= 2;
+            this.y += 2;
+            logs.add(new RotateLog(time , dir));
+        }
+
     }
 }
 
@@ -40,11 +71,9 @@ final class moveLog extends Log{
     }
 }
 final class RotateLog extends Log{
-    int units;
-    Direction direction;
-    public RotateLog(int timestamp ,int  units , Direction direction) {
+    int direction;
+    public RotateLog(int timestamp , int direction) {
         super(timestamp);
-        this.units =  units;
         this.direction = direction;
     }
 }
